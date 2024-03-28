9746452bd4aaa962ebd587f5a9d36f9d9753a48b
Alejo Herrera <alejoherrera1166@gmail.com>
2024-03-28 15:42:53 -0300

    Footer agregado.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import { Container } from "react-bootstrap";
+import "./App.css";
+import Footer from "./Components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <>
+      <Container className="mainPage"></Container>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
