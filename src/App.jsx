5a6217e4d33de9b685d06d447d7b807e3d1ac654
Alejo Herrera <alejoherrera1166@gmail.com>
2024-04-06 16:20:49 -0300

    Mostramos los datos de la API en nuestra página.

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,32 @@ import { useEffect, useState } from "react";
 function App() {
   const [personaje, setPersonaje] = useState({});
 
-  useEffect(()=>{
+  useEffect(() => {
     consultarAPI();
-  },[])
+  }, []);
 
-  const consultarAPI = async() => {
+  const consultarAPI = async () => {
     // Petición=Solicitud=Request
     // https://thesimpsonsquoteapi.glitch.me/quotes
-    const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
-    const dato = await respuesta.json();
-    console.log(respuesta)
-    console.log(dato)
-    setPersonaje(dato[0]);
-  }
+    try {
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
+      const dato = await respuesta.json();
+      setPersonaje(dato[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
       <Container className="mainPage text-center">
         <img src={logo} alt="Logo de los Simpsons" className="imgLogo my-4" />
-        <Frase></Frase>
-        <Button variant="warning" className="mt-2 mb-5">Obtener Frase</Button>
+        <Frase personaje={personaje}></Frase>
+        <Button variant="warning" className="mt-2 mb-5">
+          Obtener Frase
+        </Button>
       </Container>
       <Footer></Footer>
     </>
--- src/Components/Frase.jsx
+++ src/Components/Frase.jsx
@@ -1,23 +1,23 @@
 import { Card } from "react-bootstrap";
 
-const Frase = () => {
+const Frase = ({personaje}) => {
   return (
     <main className="my-2 p-2 bg-transparent">
       <Card className=" bg-transparent border-0">
-        <Card.Header className="h1">Nelson Muntz</Card.Header>
+        <Card.Header className="h1">{personaje.character}</Card.Header>
         <div className="text-center">
           <Card.Img
             variant="top"
-            src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
-            alt="Foto del dueño de la frase."
+            src={personaje.image}
+            alt={personaje.character}
             className="imgCard"
           ></Card.Img>
         </div>
         <Card.Text className="bg-light my-3 p-2 fs-4">
-          Shoplifting is a victimless crime, like punching someone in the dark.
+          {personaje.quote}
           <br />
           <i className="text-secondary h-6 text-wrap fs-5 p-0 m-0">
-            - Nelson Muntz in The Simpsons
+            - {personaje.character} in The Simpsons
           </i>
         </Card.Text>
       </Card>
